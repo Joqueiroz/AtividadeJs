b6985cd655345a679e6e94760b577ffe2df3db72
Joqueiroz <joaoqueiroz964@gmail.com>
2024-07-31 11:15:43 -0300

    atividades

---
+++ AtividadeMaiorMenor.js
@@ -0,0 +1,13 @@
+const num1 = 12
+const num2 = 10
+if (num1 == num2){
+    console.log('Numeros Iguais')
+}
+else{
+    if (num1 > num2){
+        console.log('O numero 1 '+num1+' é maior que '+num2+'')
+    }
+    else{
+        console.log('O numero 2 '+num2+' é maior que '+num1+'')
+    }
+}
---
+++ Nota.js
@@ -0,0 +1,12 @@
+const nota = 4
+if(nota >= 6){
+    console.log('O aluno foi aprovado')
+}
+else {
+    if(nota < 4){
+    console.log("O aluno foi reprovado")
+}
+    else{
+    console.log('O aluno esta de recuperação')
+}
+}
